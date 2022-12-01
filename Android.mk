75cc6c97695ebbf864507297720f100b282fe1b5
Harish Bandi <quic_hbandi@quicinc.com>
2022-12-01 23:08:07 +0530

    bt: add slimbus module compilation
    Adding slimbus compilation for monaco
    
    Change-Id: I860f879fd3fb49f49626a76d3189bffcef89798f

--- Android.mk
+++ Android.mk
@@ -63,14 +63,14 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
 ################################ slimbus ################################
-#include $(CLEAR_VARS)
-#LOCAL_SRC_FILES           := $(BT_SRC_FILES)
-#LOCAL_MODULE              := bt_fm_slim.ko
-#LOCAL_MODULE_KBUILD_NAME  := slimbus/bt_fm_slim.ko
-#LOCAL_MODULE_TAGS         := optional
-#LOCAL_MODULE_DEBUG_ENABLE := true
-#LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
-#include $(DLKM_DIR)/Build_external_kernelmodule.mk
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES           := $(BT_SRC_FILES)
+LOCAL_MODULE              := bt_fm_slim.ko
+LOCAL_MODULE_KBUILD_NAME  := slimbus/bt_fm_slim.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
 ################################ rtc6226 ################################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES           := $(BT_SRC_FILES)
--- bt_kernel_vendor_board.mk
+++ bt_kernel_vendor_board.mk
@@ -11,8 +11,8 @@ ifeq ($(TARGET_USES_QMAA),true)
 else
   ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
      BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko\
-	    $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
-
+		$(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
+		$(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
   endif
 endif
 endif
