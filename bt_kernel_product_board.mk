05d0213d8d52ed56c569ca06a1aa8cc771d7c3d1
Yanfei Zhou <quic_zyanfei@quicinc.com>
2023-09-01 16:16:28 +0530

    bt-kernel: Support BT power driver for new BT chip
    BT power driver "bt_power_new.ko" is built out
    for new BT chip.
    
    Change-Id: Iee17767bbe83763c6954bccf9864fe9883a2a34c
    Signed-off-by: Yanfei Zhou <quic_zyanfei@quicinc.com

--- bt_kernel_product_board.mk
+++ bt_kernel_product_board.mk
@@ -1,2 +1,5 @@
 # Build BT kernel drivers
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/btpower.ko
+ifeq ($(BOARD_HAVE_DUAL_BLUETOOTH),true)
+PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/btpower_new.ko
+endif
--- bt_kernel_vendor_board.mk
+++ bt_kernel_vendor_board.mk
@@ -1,4 +1,7 @@
 # Build audio kernel driver
 ifeq ($(call is-board-platform-in-list, msmnile gen4), true)
   BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko
+  ifeq ($(BOARD_HAVE_DUAL_BLUETOOTH),true)
+    BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower_new.ko
+  endif
 endif
