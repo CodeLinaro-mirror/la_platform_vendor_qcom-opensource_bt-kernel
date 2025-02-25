3f0977c8ecbcd5f774cf70dcfbf211f640b7c4af
Girish BN <quic_gbn@quicinc.com>
2025-02-25 15:16:36 +0530

    bt_pwr: adds pid check before signaling to client
     - This change adds logic to check client
     registration, it signals to client only
     if registered.
    
    Change-Id: Ief412b96da3f202b67b76ff33bc4c323e2c77a02
    Signed-off-by: ManiSrinivas Srungavarapu <quic_msrungav@quicinc.com>

--- pwr/btpower.c
+++ pwr/btpower.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 /*
@@ -1670,6 +1670,8 @@ static int bt_power_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	pwr_data->pdev = pdev;
+	pwr_data->reftask_bt = NULL;
+	pwr_data->reftask_uwb = NULL;
 
 	pr_info("%s: Get FMD nvmem-cells\n", __func__);
 	/* Get fmd_set NVMEM  Cell Handler */
@@ -1984,10 +1986,21 @@ int power_enable (enum SubSystem SubSystemType)
 void send_signal_to_subsystem(int SubSystemType, int state)
 {
 	pwr_data->wrkq_signal_state = state;
-	if (SubSystemType == BLUETOOTH)
+	if (SubSystemType == BLUETOOTH) {
+		if (!pwr_data->reftask_bt) {
+			pr_err("%s: BT client is not register to send signal\n",
+				__func__);
+			return;
+		}
 		queue_work(pwr_data->workq, &pwr_data->bt_wq);
-	else
+	} else {
+		if (!pwr_data->reftask_uwb) {
+			pr_err("%s: UWB client is not register to send signal\n",
+				__func__);
+			return;
+		}
 		queue_work(pwr_data->workq, &pwr_data->uwb_wq);
+	}
 }
 
 int power_disable (enum SubSystem SubSystemType)
@@ -2084,25 +2097,13 @@ static int client_state_notified(int SubSystemType)
 
 	if (SubSystemType == BLUETOOTH) {
 		update_sub_state(SSR_ON_BT);
-		if (get_pwr_state() == ALL_CLIENTS_ON) {
-			if (!pwr_data->reftask_uwb) {
-				pr_err("%s: UWB PID is not register to send signal\n",
-					__func__);
-				return -1;
-			}
+		if (get_pwr_state() == ALL_CLIENTS_ON)
 			send_signal_to_subsystem(UWB, SSR_ON_BT);
-		}
 	} else {
 		update_sub_state(SSR_ON_UWB);
-		if (get_pwr_state() == ALL_CLIENTS_ON) {
-			if (!pwr_data->reftask_bt) {
-				pr_err("%s: BT PID is not register to send signal\n",
-					__func__);
-				return -1;
-			}
+		if (get_pwr_state() == ALL_CLIENTS_ON)
 			send_signal_to_subsystem(BLUETOOTH,
 				(SIGIO_NOTIFICATION_SIGNAL|SIGIO_SSR_ON_UWB));
-		}
 	}
 	return 0;
 }
