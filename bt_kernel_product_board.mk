1c7b663241bd2540c8c3f76f366d3e4348a9908f
Dongliang Cao <quic_dongcao@quicinc.com>
2025-03-17 14:59:26 +0530

    [NO MERGE] Bluetooth: Un-install BT driver
    Un-install BT driver to unblock Android W(16) bring-up.
    
    Change-Id: I1494086d0248ea79a587eac7ef2b5cac59147906
    Signed-off-by: Dongliang Cao <quic_dongcao@quicinc.com>

--- bt_kernel_product_board.mk
+++ bt_kernel_product_board.mk
@@ -1,5 +1,7 @@
 # Build BT kernel drivers
+ifneq ($(filter $(PLATFORM_VERSION), 16 Baklava),$(PLATFORM_VERSION))
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/btpower.ko\
 	$(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
 	$(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
+endif
 
