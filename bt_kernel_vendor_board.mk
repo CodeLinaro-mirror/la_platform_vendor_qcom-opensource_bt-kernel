460a7aefd56325c83ace1b3e99cb0943be1e53c8
Yanfei Zhou <zyanfei@qti.qualcomm.com>
2025-04-15 10:40:33 +0530

    bt-kernel: Remove un-used folders
    1.  rtc6226
    2.  slimbus
    3.  btfmcodec
    
    Change-Id: Iac5367b9bfcea1c9051b6d3447b5891230e04f9c
    Signed-off-by: Yanfei Zhou <zyanfei@qti.qualcomm.com>
    (cherry picked from commit 2f2940a86a33ec223193d8efe5cf8ff7d0ca5eb0)

--- bt_kernel_vendor_board.mk
+++ bt_kernel_vendor_board.mk
@@ -1,23 +1,4 @@
 # Build audio kernel driver
-ifneq ($(TARGET_BOARD_AUTO),true)
-ifeq ($(TARGET_USES_QMAA),true)
-  ifeq ($(TARGET_USES_QMAA_OVERRIDE_BLUETOOTH), true)
-     ifneq (,$(call is-board-platform-in-list2,$(TARGET_BOARD_PLATFORM)))
-         BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko\
-             $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
-	     $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
-     endif
-  endif
-else
-  ifneq (,$(call is-board-platform-in-list2,$(TARGET_BOARD_PLATFORM)))
-     BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko\
-            $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
-	    $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
-
-  endif
-endif
-else
-  ifneq (,$(call is-board-platform-in-list2, msmnile sm6150 gen4))
-    BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko
-  endif
+ifneq (,$(call is-board-platform-in-list2, msmnile sm6150 gen4))
+  BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko
 endif
