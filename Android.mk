11a227eea3ab896d8347fdd470b650098b70d0dd
Adesh Mohanrao Pathare <adesmoha@qti.qualcomm.com>
2026-04-22 10:52:14 +0530

    bt-kernel: Added fix to check thread support
    Added fix to check if the thread feature flag is supported
    then only compile the thread kernel module.
    
    CRs-Fixed: 4511256
    Change-Id: I72a6e7335e731f8cac4eeb241cacaedf44c24717

--- Android.mk
+++ Android.mk
@@ -219,6 +219,7 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
 ################################  thqspi driver################################
+ifeq ($(BOARD_HAVE_STANDALONE_THREAD), true)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES           := $(BT_SRC_FILES)
 LOCAL_MODULE              := thqspi_proto.ko
@@ -227,6 +228,7 @@ LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
+endif
 ###########################################################
 
 endif # DLKM check
